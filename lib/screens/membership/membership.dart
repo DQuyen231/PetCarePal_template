a65c2d24ae4607f821f4f47414d5c15c9a5215b5
DQuyen231 <98691014+DQuyen231@users.noreply.github.com>
2024-04-04 11:18:30 +0700

    add more pages

---
+++ lib/screens/membership/membership.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Membership extends StatefulWidget {
+  const Membership({super.key});
+
+  @override
+  State<Membership> createState() => _MembershipState();
+}
+
+class _MembershipState extends State<Membership> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('membership');
+  }
+}
